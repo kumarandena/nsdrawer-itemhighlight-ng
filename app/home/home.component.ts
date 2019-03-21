188fabc79a6faf59d161136b2b06c7230a77c15c
kumarandena <kumarandena@gmail.com>
2019-03-22 00:43:18 +0530

    kick-off

---
+++ app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { isAndroid } from "tns-core-modules/platform";
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+
+    actionAndroid;
+
+    constructor() {
+        this.actionAndroid = isAndroid;
+    }
+
+    ngOnInit(): void {
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
